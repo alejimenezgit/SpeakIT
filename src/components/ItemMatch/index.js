71d82b1696a83b5fe28617c4280b1e61df2051fb
alejandro <alejimenezgitgithub@gmail.com>
2020-05-30 20:30:24 +0200

    match

--- src/components/ItemMatch/index.js
+++ src/components/ItemMatch/index.js
@@ -1,40 +1,10 @@
 import React from 'react';
 import "./styles.scss";
 
-import apiClientUser from "../../services/users";
-import apiClientComunications from "../../services/comunication";
-
 export default  class ItemMatch extends React.Component {
 
-    addUser = (id) => {
-        console.log(id);
-        apiClientComunications
-            .update(id, {status: 'match'})
-            .then((result) => {this.updateAllMatches()})
-            .catch(() => {})
-    }
-
-    
-
-    refuseUser = () => {
-        console.log('refuse')
-    }
-
-    updateAllMatches = () => {
-        apiClientUser
-            .oneUserMatches(this.props.user._id,{status: 'match'})
-            .then((users) => {
-                this.setState({
-                    allMatches: users.data,
-                    isloading: false
-                })
-                console.log('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuusers', users.data)
-            })
-            .catch(()=> this.setState({ error: true }))
-    }
-
     render(){
-        const {index,user} = this.props;
+        const {index,user, addUser, refuseUser } = this.props;
         return <div key={index}> 
                     {user.state !== 'match' && <img width="40" src="https://devshift.biz/wp-content/uploads/2017/04/profile-icon-png-898.png" alt="imgprofile"/>}
                     {user.state !== 'match' && user.name}
@@ -42,10 +12,10 @@ export default  class ItemMatch extends React.Component {
                     {user.state === 'addOrNot' && 
                         <div> 
                             <div> 
-                                <button onClick={() => this.addUser(user.idCom)}> ADD </button> 
+                                <button onClick={addUser(user.idCom)}> ADD </button> 
                             </div>
                             <div> 
-                                <button onClick={() => this.refuseUser(user.idCom)}> REFUSE </button> 
+                                <button onClick={refuseUser(user.idCom)}> REFUSE </button> 
                             </div>
                         </div>
                     }
--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -3,6 +3,7 @@ import "./styles.scss";
 
 import ItemMatch from "../../components/ItemMatch"
 
+import apiClientComunications from "../../services/comunication";
 import apiClientUser from "../../services/users";
 
 export default class Match extends React.Component {
@@ -14,10 +15,30 @@ export default class Match extends React.Component {
     }
 
     componentDidMount() {
+        this.usersMatches();
+    }
+
+    addUser = (id) => {
+        console.log(id);
+        apiClientComunications
+            .update(id, {status: 'match'})
+            .then((result) => {this.updateAllMatches()})
+            .catch(() => {})
+    }
+
+    
+
+    refuseUser = () => {
+        console.log('refuse')
+    }
+    
+    usersMatches = () => {
         this.setState({ isloading: true });
+        console.log('props', this.props)
         apiClientUser
             .oneUserMatches(this.props.user._id,{status: 'match'})
             .then((users) => {
+                console.log(users);
                 this.setState({
                     allMatches: users.data,
                     isloading: false
@@ -26,12 +47,12 @@ export default class Match extends React.Component {
             })
             .catch(()=> this.setState({ error: true }))
     }
-    
+
     allMatches() {
        console.log('state', this.state.allMatches);
        
        return this.state.allMatches.map((user, index) => {
-            return  <ItemMatch index={index} user={user}/> 
+            return  <ItemMatch index={index} user={user} addUser={this.addUser()} refuseUser={this.refuseUser()}/> 
        })
     }
     renderMatch = () => {
